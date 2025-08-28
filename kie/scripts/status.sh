e6af1c283d39432fbd6eca263d766c71257704c6
nkusla <nikola.kuslakovic1616@gmail.com>
2025-08-28 20:07:46 +0200

    chore: Removed unused scripts

--- kie/scripts/status.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "📋 Checking existing containers..."
-curl -s -u admin:admin http://localhost:8080/kie-server/services/rest/server/containers || { echo "❌ Failed to fetch containers"; exit 1; }
-echo ""
